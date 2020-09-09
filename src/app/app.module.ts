1d70c684f4dced07ae8236f8cfb187f0dd7a528b
Guy Levin <gena.levin@gmail.com>
2020-09-09 09:36:23 +0300

    Init NG app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { QuestionComponent } from './components/question/question.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
